e678801a4207eb179a6aea1d81c2ca4c1a5e9d9e
Ana-Keilly <okeila109@gmail.com>
2025-11-12 09:33:27 -0300

    Update cadastrarProduto.php

--- sisCadProduto/cadastrarProduto.php
+++ sisCadProduto/cadastrarProduto.php
@@ -1,44 +1,40 @@
-<?php
-    
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <link rel="stylesheet" type="text/css" href="estilo.css">
-    <title>SisCadProduto</title>
 </head>
 <body>
     <nav>
         <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="cadastrarProduto.php">Cadastrar</a></li>
-        <li><a href="consultarProduto.php">Consultar</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="cadastrarProduto.php">Cadastrar</a></li>
+            <li><a href="consultarProdutos.php">Consultar</a></li>
         </ul>
+
     </nav>
-        <h1>cadastrar produto</h1>
+</header>
 
-        <form method="POST" action="cadastrarProdutoControl.php">
-            <label for="">nome produto;</label>
-            <input type="text" name="nome">
-            <br><br>
+<h1>Cadastrar Produto</h1>
 
-            <label for="">valor produto;</label>
-            <input type="text" name="valor">
-            <br><br>
-
-            <label for="">quantidade produto;</label>
-            <input type="number" name="quantidade">
-            <br><br>
-
-            <label for="">descricao produto;</label>
-            <input type="text" name="descricao">
-            <br><br>
-
-            <input type="submit" value="cadastrar" name="btnCadastrar"/>
-        </form>
+<form method="POST" action="cadastrarProdutoControl.php">
+<label>Nome do Produto: </label>    
+<input type="text" name="nome"/>
+    <br><br>
+    <label>Quantidade do Produto: </label>    
+    <input type="number" name="quantidade"/>
+    <br><br>
+    <label>Valor do Produto: </label>    
+    <input type="text" name="valor"/>
+    <br><br>
+    <label>Descrição do Produto: </label>    
+    <input type="text" name="descricao"/>
+    <br><br>
+    <input type="submit" value="Cadastrar" name="btnCadastrar"/>
+</form>
 
 </body>
 </html>
+           
